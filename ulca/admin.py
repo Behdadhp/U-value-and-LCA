2b58edbd1bfcf50e2bb18e8e0cd06be6ae0188a7
Behdad Hajipour <behdad.hajipour95@gmail.com>
2023-04-21 02:03:55 +0200

    add admin to the model

--- ulca/admin.py
+++ ulca/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from . import models
+
+
+class BuildingAdmin(admin.ModelAdmin):
+    """Class representing of Building admin page"""
+
+    list_display = ("name", "project")
+
+
+admin.site.register(models.Building, BuildingAdmin)
