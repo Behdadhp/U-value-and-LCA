fdd52769cdf5ecb29b09f8763b2e6e221b9555b0
Behdad Hajipour <behdad.hajipour95@gmail.com>
2023-04-23 00:43:02 +0200

    add u-values to admin

--- ulca/admin.py
+++ ulca/admin.py
@@ -6,7 +6,7 @@ from . import models
 class BuildingAdmin(admin.ModelAdmin):
     """Class representing of Building admin page"""
 
-    list_display = ("name", "project", "wall", "roof", "floor")
+    list_display = ("name", "project", "wall", "roof", "floor", "wallUvalue", "roofUvalue", "floorUvalue")
 
 
 admin.site.register(models.Building, BuildingAdmin)
