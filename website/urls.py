31c5e55cf84654626a0cd6b8ade38bd5df157283
Behdad Hajipour <behdad.hajipour95@gmail.com>
2023-04-26 23:29:56 +0200

    add index page

--- website/urls.py
+++ website/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", views.index.as_view(), name="index"),
     path("", include("ulca.urls", namespace="building")),
 ]
