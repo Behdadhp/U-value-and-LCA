54d0657876222283f677f5af63a6f5b31b581b5c
Behdad Hajipour <behdad.hajipour95@gmail.com>
2023-04-26 22:35:17 +0200

    update url to buildings/...

--- ulca/urls.py
+++ ulca/urls.py
@@ -5,7 +5,7 @@ app_name = "building"
 
 urlpatterns = [
     path("buildings/", views.BuildingList.as_view(), name="buildings"),
-    path("building/<int:pk>", views.BuildingDetails.as_view(), name="details"),
-    path("building/new/", views.BuildingCreate.as_view(), name="create"),
-    path("building/delete/<int:pk>", views.BuildingDelete.as_view(), name="delete"),
+    path("buildings/<int:pk>", views.BuildingDetails.as_view(), name="details"),
+    path("buildings/new/", views.BuildingCreate.as_view(), name="create"),
+    path("buildings/delete/<int:pk>", views.BuildingDelete.as_view(), name="delete"),
 ]
