67e4b5085f41f95831e9bd43f5d2431f2a9dfca7
Behdad Hajipour <behdad.hajipour95@gmail.com>
2023-05-17 23:39:22 +0200

    update column GWP in tables.py

--- ulca/tables.py
+++ ulca/tables.py
@@ -93,10 +93,10 @@ class MaterialTable(tables.Table):
     @staticmethod
     def render_items():
         items = [
-            "Herstellungsphase",
-            "Erneuerung",
-            "Energiebedarf",
-            "Lebensendphase",
+            "Herstellungsphase (A1 to A3)",
+            "Erneuerung (B2 and B4)",
+            "Energiebedarf (B6)",
+            "Lebensendphase (C3 and C4)",
         ]
         return format_html_join("\n", "<p>{}: </p>", ((key,) for key in items))
 
