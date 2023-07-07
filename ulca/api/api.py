a7735cce8b3f748ad5085d54c4ca83f4f9fad96a
Behdad Hajipour <behdad.hajipour95@gmail.com>
2023-07-07 14:26:46 +0200

    add end point for building model

---
+++ ulca/api/api.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI
+import os
+import django
+from django.core import serializers
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "website.settings")
+django.setup()
+
+from ulca import models
+
+app = FastAPI()
+
+
+@app.get("/building-model/", tags=["building"])
+async def building_model():
+    """Endpoint to get building model"""
+
+    model = models.Building.objects.all()
+    serialized_model = serializers.serialize("json", model)
+    return serialized_model
+
+
+@app.get("/building-model/{building_id}", tags=["building"])
+def building_model_id(building_id: int):
+    """Endpoint to get one building"""
+
+    model = models.Building.objects.get(id=building_id)
+    return model
--- website/settings.py
+++ website/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-ykks3!sz4+d%+(1ub#pl^^b^l6jlcb01-aoqcw=-973c4-+jbp
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1"]
 
 
 # Application definition
